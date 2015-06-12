245cd2c1046d86c09e5dabb7730def09c7aca3b1
grese <grese@yahoo-inc.com>
2015-06-12 15:30:08 -0700

    hiding headers when no content

--- dist/ember-bootstrap-table.js
+++ dist/ember-bootstrap-table.js
@@ -389,28 +389,6 @@ var define, requireModule, require, requirejs;
 
     function program1(depth0,data) {
       
-      var buffer = '', stack1;
-      data.buffer.push("\n    <div ");
-      data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
-        'class': (":table-component-sticky-header-container stickyHeaderActive:sticky")
-      },hashTypes:{'class': "STRING"},hashContexts:{'class': depth0},contexts:[],types:[],data:data})));
-      data.buffer.push(">\n        <div class=\"container table-component-sticky-header-inner\">\n        ");
-      stack1 = helpers['if'].call(depth0, "_stickyTable", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0],types:["ID"],data:data});
-      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n        </div>\n    </div>\n");
-      return buffer;
-      }
-    function program2(depth0,data) {
-      
-      var buffer = '';
-      data.buffer.push("\n            ");
-      data.buffer.push(escapeExpression(helpers.view.call(depth0, "_stickyTable", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data})));
-      data.buffer.push("\n        ");
-      return buffer;
-      }
-
-    function program4(depth0,data) {
-      
       var buffer = '';
       data.buffer.push("\n    ");
       data.buffer.push(escapeExpression(helpers.view.call(depth0, "noContentView", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data})));
@@ -418,32 +396,60 @@ var define, requireModule, require, requirejs;
       return buffer;
       }
 
-    function program6(depth0,data) {
+    function program3(depth0,data) {
       
-      var buffer = '';
-      data.buffer.push("\n            ");
-      data.buffer.push(escapeExpression(helpers.view.call(depth0, "_table", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data})));
-      data.buffer.push("\n        ");
-      return buffer;
-      }
-
-      stack1 = helpers['if'].call(depth0, "stickyHeader", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data});
+      var buffer = '', stack1;
+      data.buffer.push("\n    ");
+      stack1 = helpers['if'].call(depth0, "stickyHeader", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0],types:["ID"],data:data});
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n\n");
-      stack1 = helpers['if'].call(depth0, "_showNoContentView", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0],types:["ID"],data:data});
-      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n\n<div ");
+      data.buffer.push("\n    <div ");
       data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
         'class': (":table-component-table-container _showNoContentView:hidden")
       },hashTypes:{'class': "STRING"},hashContexts:{'class': depth0},contexts:[],types:[],data:data})));
-      data.buffer.push(">\n    <div ");
+      data.buffer.push(">\n        <div ");
       data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
         'class': ("responsive:table-responsive")
       },hashTypes:{'class': "STRING"},hashContexts:{'class': depth0},contexts:[],types:[],data:data})));
-      data.buffer.push(">\n        ");
-      stack1 = helpers['if'].call(depth0, "_table", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(6, program6, data),contexts:[depth0],types:["ID"],data:data});
+      data.buffer.push(">\n            ");
+      stack1 = helpers['if'].call(depth0, "_table", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(7, program7, data),contexts:[depth0],types:["ID"],data:data});
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n    </div>\n</div>\n");
+      data.buffer.push("\n        </div>\n    </div>\n");
+      return buffer;
+      }
+    function program4(depth0,data) {
+      
+      var buffer = '', stack1;
+      data.buffer.push("\n        <div ");
+      data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
+        'class': (":table-component-sticky-header-container stickyHeaderActive:sticky")
+      },hashTypes:{'class': "STRING"},hashContexts:{'class': depth0},contexts:[],types:[],data:data})));
+      data.buffer.push(">\n            <div class=\"container table-component-sticky-header-inner\">\n                ");
+      stack1 = helpers['if'].call(depth0, "_stickyTable", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0],types:["ID"],data:data});
+      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+      data.buffer.push("\n            </div>\n        </div>\n    ");
+      return buffer;
+      }
+    function program5(depth0,data) {
+      
+      var buffer = '';
+      data.buffer.push("\n                    ");
+      data.buffer.push(escapeExpression(helpers.view.call(depth0, "_stickyTable", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data})));
+      data.buffer.push("\n                ");
+      return buffer;
+      }
+
+    function program7(depth0,data) {
+      
+      var buffer = '';
+      data.buffer.push("\n                ");
+      data.buffer.push(escapeExpression(helpers.view.call(depth0, "_table", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data})));
+      data.buffer.push("\n            ");
+      return buffer;
+      }
+
+      stack1 = helpers['if'].call(depth0, "_showNoContentView", {hash:{},hashTypes:{},hashContexts:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data});
+      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+      data.buffer.push("\n");
       return buffer;
       
     });
